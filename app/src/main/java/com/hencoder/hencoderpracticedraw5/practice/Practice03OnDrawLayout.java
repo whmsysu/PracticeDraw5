ad5d40cc984bf365766b3121ad98b98e49fa9a8a
Haomin Wu <wuhaominsysu@gmail.com>
2017-11-22 18:22:37 -0500

    Finish Practice 5

--- app/src/main/java/com/hencoder/hencoderpracticedraw5/practice/Practice03OnDrawLayout.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw5/practice/Practice03OnDrawLayout.java
@@ -26,6 +26,7 @@ public class Practice03OnDrawLayout extends LinearLayout {
 
     {
         // 在这里插入 setWillNotDraw(false) 以启用完整的绘制流程
+        setWillNotDraw(false);
     }
 
     @Override
@@ -58,8 +59,8 @@ public class Practice03OnDrawLayout extends LinearLayout {
         }
 
         private void draw(Canvas canvas) {
-            int repitition = (int) Math.ceil((float) getWidth() / getHeight());
-            for (int i = 0; i < spots.length * repitition; i++) {
+            int repetition = (int) Math.ceil((float) getWidth() / getHeight());
+            for (int i = 0; i < spots.length * repetition; i++) {
                 Practice03OnDrawLayout.Pattern.Spot spot = spots[i % spots.length];
                 canvas.drawCircle(i / spots.length * getHeight() * PATTERN_RATIO + spot.relativeX * getHeight(), spot.relativeY * getHeight(), spot.relativeSize * getHeight(), patternPaint);
             }
